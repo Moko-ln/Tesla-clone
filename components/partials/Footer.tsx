29a342f2bda7446d07948ac2d30a943fbcfd3e16
Moko-ln <sergelema.web@gmail.com>
2023-01-26 22:17:24 +0100

    Bug foooter and height viewport

--- components/partials/Footer.tsx
+++ components/partials/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <footer className="lg:max-2xl:absolute bottom-0 flex items-center bg-transparent w-full px-10 py-4 text-sm">
+        <footer className="2xl:absolute xl:absolute lg:absolute bottom-0 flex items-center bg-transparent w-full px-10 py-4 text-sm">
             <div className="relative w-full">
                 <p className="md:max-xl:mb-7 md:max-lg:text-center xl:absolute bottom-0 left-0 text-center">Pensez à covoiturer #SeDéplacerMoinsPolluer</p>
                 <ul className="md:flex-row md:max-2xl:gap-5 items-center flex flex-col grow justify-center gap-2">
