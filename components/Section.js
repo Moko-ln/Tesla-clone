480ff725f54a1fc370595c742d213ea0e125c473
Moko-ln <sergelema.web@gmail.com>
2023-01-29 20:50:21 +0100

    Bug scroll 8

--- components/Section.js
+++ components/Section.js
@@ -17,7 +17,7 @@ const Section = ({data}) => {
     
     return (
             <motion.section 
-                className="w-full h-screen bg-cover bg-center bg-no-repeat snap-center"
+                className="w-full h-screen bg-cover bg-center bg-no-repeat"
                 style={{ backgroundImage: `url(./${data.imgUrl[imagebg]})` }}
             >
                 <Wrapper infosCar={data} />
